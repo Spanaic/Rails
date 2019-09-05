813c4b9406db5c83f1b2944c4f6c9821ed20c99f
Takeru Yamanaka <Takeru@takeru-no-MacBook-Pro-Retina.local>
2019-09-05 15:22:06 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '94d181f67cf606de8dbea3b6a8a2cce55cfce1950f4f2d8b5fc86bdb9d711266a869ae345b1a2f1180d65a227e584bf4be04b8b89dbea065f341f8b81b3cddcc'
