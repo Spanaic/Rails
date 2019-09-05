cf5fb443bf190414133a5565777b560358d046f4
Takeru Yamanaka <Takeru@takeru-no-MacBook-Pro-Retina.local>
2019-09-05 12:34:01 +0900

    [finish]3-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,6 +4,17 @@ class TodolistsController < ApplicationController
   end
 
   def create
-    
+
+    list = List.new(list_params)
+
+    list.save
+
+    redirect_to '/top'
   end
+  private
+
+  def list_params
+      params.require(:lists).permit(:title, :body)
+  end
+  
 end
