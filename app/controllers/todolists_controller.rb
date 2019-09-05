5ad2ac662510e1a9776aed0e32a0585bee8238a1
Takeru Yamanaka <Takeru@takeru-no-MacBook-Pro-Retina.local>
2019-09-05 13:49:03 +0900

    [finish]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -9,12 +9,21 @@ class TodolistsController < ApplicationController
 
     list.save
 
-    redirect_to '/top'
+    redirect_to todolist_path(list.id)
   end
+  
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+  
   private
 
   def list_params
-      params.require(:lists).permit(:title, :body)
+      params.require(:list).permit(:title, :body)
   end
   
 end
